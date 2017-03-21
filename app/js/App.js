d7bd29d9708658564add578dd6fa93671c9dca79
avadarad <radu.avadanii@gmail.com>
2017-03-21 18:33:18 +0200

    made the pie chart

---
+++ app/js/App.js
@@ -0,0 +1,13 @@
+var $percent = $('.pie__percent');
+var $percentText = $('.features__title span');
+
+$percent.each(function(e) {
+  var percent = parseFloat($(this).attr('data-percent'));
+  if(percent < 0) {
+    percent = 0;
+  } else if( percent > 100) {
+    percent = 100;
+  }
+  $(this).parent('.pie').attr('style', 'animation-delay: -'+percent+'s');
+  $($percentText[e]).text(percent + '%');
+});
